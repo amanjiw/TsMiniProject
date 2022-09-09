8d08cc45c5c4aae52288fc89524993382541c910
amanj ghaderi <ghaderi.amanj@gmail.com>
2022-09-09 11:15:36 +0430

    updated

--- src/app.ts
+++ src/app.ts
@@ -169,6 +169,39 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 ///////////
 //////////////////////////
 ////////////////////////////////////////
+
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 Person";
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure = (): void => {};
+
+  renderContent = (): void => {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent =
+      this.persons + "Persons assingend.";
+    this.element.querySelector("p")!.textContent = this.project.description;
+  };
+}
+
+///////////
+//////////////////////////
+////////////////////////////////////////
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   aasignedProjects: Project[] = [];
 
@@ -202,9 +235,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     )! as HTMLUListElement;
     listEl.innerHTML = "";
     for (const prjItem of this.aasignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector("ul")!.id, prjItem);
     }
   };
 
